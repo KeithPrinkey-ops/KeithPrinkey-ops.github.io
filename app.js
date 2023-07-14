7561cb5bb3b377126423327ee30c3f8eb50c64aa
Chief Technical Officer <kaprinkey1@cougars.ccis.edu>
2023-07-14 18:55:47 -0500

    Update app.js
    added form submission processing and validation

--- app.js
+++ app.js
@@ -30,3 +30,69 @@ const toggle = document.getElementById('consent');
 toggle.addEventListener('change', (e) => {
     toggle.value = e.target.checked ? 1 : 2;
 })
+
+// Check if the form is submitted
+if (document.querySelector('form').addEventListener) {
+  document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Initialize error message variable
+    let errorMessage = '';
+
+    // Retrieve form data and perform validation/sanitization
+    const name = document.querySelector('#name').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const reason = document.querySelector('#reason').value.trim();
+    const service = document.querySelector('#service').value;
+    const consent = document.querySelector('#consent').checked;
+
+    // Perform form validation
+    if (name === '') {
+      errorMessage = 'Name is required.';
+    } else if (email === '') {
+      errorMessage = 'Email is required.';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errorMessage = 'Invalid email format.';
+    } else if (reason === '') {
+      errorMessage = 'Reason for contact is required.';
+    } else if (!consent) {
+      errorMessage = 'You must consent to be contacted.';
+    }
+
+    // Process the form if there are no errors
+    if (errorMessage === '') {
+      // Prepare the request data
+      const formData = new FormData();
+      formData.append('name', name);
+      formData.append('email', email);
+      formData.append('reason', reason);
+      formData.append('service', service);
+      formData.append('consent', consent);
+
+      // Send a POST request to the server
+      fetch('form-handler.php', {
+        method: 'POST',
+        body: formData,
+      })
+        .then(function (response) {
+          if (response.ok) {
+            // Insertion successful
+            // Redirect back to the form page with a success query parameter
+            window.location.href = 'contact.html?success=true';
+          } else {
+            // Insertion failed
+            // Redirect back to the form page with an error query parameter
+            window.location.href = 'contact.html?error=' + encodeURIComponent(
+              'Error submitting the form. Please try again.'
+            );
+          }
+        })
+        .catch(function (error) {
+          console.error('Error:', error);
+        });
+    } else {
+      // Redirect back to the form page with an error query parameter
+      window.location.href = 'contact.html?error=' + encodeURIComponent(errorMessage);
+    }
+  });
+}
